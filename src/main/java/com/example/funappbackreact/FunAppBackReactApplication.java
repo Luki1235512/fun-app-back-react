204156761c22a1e2681cc8fd50f58b704b784fff
Luki1235512 <Luki1235512@gmail.com>
2022-02-02 13:27:57 +0100

    Created app

---
+++ src/main/java/com/example/funappbackreact/FunAppBackReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.funappbackreact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunAppBackReactApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FunAppBackReactApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/funappbackreact/FunAppBackReactApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.funappbackreact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunAppBackReactApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
