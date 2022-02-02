3b4c8f2293a9a4428350bdda75716cfee49e9c0b
Luki1235512 <Luki1235512@gmail.com>
2022-02-02 14:04:16 +0100

    Basic employee model to check if it works

---
+++ src/main/java/com/example/funappbackreact/controller/EmployeeController.java
@@ -0,0 +1,23 @@
+package com.example.funappbackreact.controller;
+
+import com.example.funappbackreact.model.Employee;
+import com.example.funappbackreact.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployees() {
+        return employeeRepository.findAll();
+    }
+}
